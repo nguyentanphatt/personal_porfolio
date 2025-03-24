33f537ff88d016a9c0b3e3f285af8d5e1713d115
nguyentanphatt <bloodomega97@gmail.com>
2025-03-24 18:08:06 +0700

    fix: navbar flow

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,10 @@ import Navbar from "@/sections/Navbar";
 import ProjectSection from "@/sections/ProjectSection";
 import Tape from "@/sections/Tape";
 import TechSection from "@/sections/TechSection";
-import { useRef } from "react";
+//import { useRef } from "react";
 
 export default function Home() {
-  const homeRef = useRef<HTMLElement>(null);
+  /* const homeRef = useRef<HTMLElement>(null);
   const techRef = useRef<HTMLElement>(null);
   const projectRef = useRef<HTMLElement>(null);
   const contactRef = useRef<HTMLElement>(null);
@@ -22,23 +22,15 @@ export default function Home() {
       component: <ProjectSection />,
     },
     { id: "contact", name: "Contact", ref: contactRef, component: <Footer /> },
-  ];
+  ]; */
   return (
     <div className="">
-      <Navbar
-        links={sections.map((s) => ({ href: s.id, name: s.name }))}
-        sectionRefs={
-          sections.map((s) => s.ref) as React.RefObject<HTMLElement>[]
-        }
-      />
-      {sections.map((s) => (
-        <div key={s.id}>
-          <section id={s.id} ref={s.ref}>
-            {s.component}
-          </section>
-          {s.id === "tech" && <Tape />}
-        </div>
-      ))}
+      <Navbar />
+      <Hero />
+      <TechSection/>
+      <Tape />
+      <ProjectSection />
+      <Footer />
     </div>
   );
 }
